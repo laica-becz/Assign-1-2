08538bb976d97adbc98c53780ae7e773967a466d
laica-becz <zepedabiancaellise@gmail.com>
2025-12-03 20:03:37 +0800

    Update Assign 3.php

--- Assign 3.php
+++ Assign 3.php
@@ -8,9 +8,20 @@ $ingredients = ['Almond Butter' => ['price' => 390.00, 'stock' => 10],
                 'Chocolate Frosting' => ['price' => 100.00, 'stock' => 7]
             ]; //Step 2
 //Step 3
-$tax_rate = 0.12; //12% VAT 
+$tax_rate = 12; //12% VAT 
 
 //Step 4
 function get_reorder_message(int $stock_level): string {
     return $stock_level < 5 ? 'Yes' : 'No';
 }
+
+//Step 5
+function get_total_value(float $price, int $quantity, int $tax_rate): float {
+    return ($price * $quantity) * (1 + $tax_rate);
+}
+
+//Step 6
+function get_tax_due(float $price, int $quantity, int $tax_rate): float {
+    return ($price * $quantity) * ($tax_rate / 100);
+}
+?>
