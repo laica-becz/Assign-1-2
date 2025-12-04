9ec54a9f6ec9c580784da825148aa726daaf810c
laica-becz <zepedabiancaellise@gmail.com>
2025-12-04 10:12:37 +0800

    Refactor get_total_value and update get_tax_due

--- Assign 3.php
+++ Assign 3.php
@@ -16,13 +16,13 @@ function get_reorder_message(int $stock_level): string {
 }
 
 //Step 5
-function get_total_value(float $price, int $quantity, int $tax_rate): float {
-    return ($price * $quantity) * (1 + $tax_rate);
+function get_total_value(float $price, int $quantity): float {
+    return ($price * $quantity);
 }
 
 //Step 6
 function get_tax_due(float $price, int $quantity, int $tax_rate): float {
-    return ($price * $quantity) * ($tax_rate / 100);
+    return ($price * $quantity) * (1 + $tax_rate / 100);
 }
 ?>
 <!DOCTYPE html>
